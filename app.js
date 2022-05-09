224b91d79eab77a366345b9f40b712377f9964cb
Brien Thomas <thomasbrien94@gmail.com>
2022-05-09 14:09:50 -0700

    pulled list data from spotlight

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 // import functions and grab DOM elements
+import { landscapesArray } from './landscape-data.js';
 
+import { renderLandscape } from './utils.js';
+
+const landscapesListEl = document.getElementById('landscapes-list');
 // let state
+for (let landscape of landscapesArray) {
+    const landscapeDiv = renderLandscape(landscape);
+    landscapesListEl.append(landscapeDiv);
+}
 
 // set event listeners 
   // get user input
