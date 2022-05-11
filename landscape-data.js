69b002bf21d67ca54991ce0e7161582dd4f40ffe
Brien Thomas <thomasbrien94@gmail.com>
2022-05-10 17:35:56 -0700

    gave up on pulling photos from the array with a script. Display formatting changed for the ID page

--- landscape-data.js
+++ landscape-data.js
@@ -3,32 +3,32 @@ export const landscapesArray = [
         id: '1',
         name: 'Latourell Falls',
         image: './assets/Latourell Falls 1.jpg',
-        details: 'A wooded area with hiking trails throughout, notable features include a bridge spanning a small river.',
+        details: 'A wooded area with hiking trails throughout, notable features include multiple waterfalls along the route!',
         shyEnjoyment: 'Shy says: "What a great place to visit! So many smells! Squirrel?!"',
-        photos: ['./assets/Latourell Falls 1.jpg', './assets/Latourell Falls 2.jpg', './assets/Latourell Falls 3.jpg', './assets/Latourell Falls 4.jpg'],
+        photos: ['./assets/Latourell Falls.jpg', './assets/Latourell Falls 2.jpg', './assets/Latourell Falls 3.jpg'],
     },
     {
         id: '2',
         name: 'Table Rock',
         image: './assets/Table Rock 4.jpg',
-        details: 'Lakefront camping with Mt. Hood just a stone\'s throw away; don\'t forget your swimming suit!',
-        shyEnjoyment: '"Another fun place! Are you sure the water\'s sanitary? It looks questionable to me! -Cheyanne',
-        photos: ['./assets/Table Rock 1.jpg', './assets/Table Rock 2.jpg', './assets/Table Rock 3.jpg', './assets/Table Rock 4.jpg'],
+        details: '30 minutes past the point of no cell service, down a rocky, pitted dirt road. Geocachers will have fun looking for the cache slightly off-trail. A rather difficult hike due to the steep incline near the end. Watch for snakes!',
+        shyEnjoyment: '"Soooo hot... Did you bring water? I can climb that hill faster than you!! -Cheyanne',
+        photos: ['./assets/Table Rock 1.jpg', './assets/Table Rock 2.jpg', './assets/Table Rock 3.jpg'],
     },
     {
         id: '3',
         name: 'Neah-kah-nie Mountain Trail',
         image: './assets/Neah-kah-nie Mountain Trail 2.jpg',
-        details: 'This remote spot on the Oregon coast offers a wonderful view of the sunset.',
+        details: 'This hike is short, but still difficult for the inexperienced. It culminates at the peak of the small mountain, allowing for a great view on a clear day. It\'s also very windy up top!',
         shyEnjoyment: 'Shy\'s take on the beach: "This water is even more questionable! It tried to get me! Hey, can I eat that crab?"',
-        photos: ['./assets/Neah-kah-nie Trail.jpg', './assets/Neah-kah-nie Trail 2.jpg', './assets/Neah-kah-nie Trail 3.jpg'],
+        photos: ['./assets/Neah-kah-nie Mountain Trail.jpg', './assets/Neah-kah-nie Mountain Trail 1.jpg', './assets/Neah-kah-nie Mountain Trail 2.jpg'],
     },
     {
         id: '4',
         name: 'Warrior Rock Lighthouse Trail',
         image: './assets/Warrior Rock Lighthouse Trail 3.jpg',
-        details: 'Blah blah',
-        shyEnjoyment: 'Wow, very hot, much mosquito',
-        photos: ['./assets/Warrior Rock Lighthouse Trail 1.jpg', './assets/Warrior Rock Lighthouse Trail 2.jpg', './assets/Warrior Rock Lighthouse Trail 3.jpg']
+        details: 'On Sauvie Island, this hike has relatively no elevation gain or loss, and ends at the lighthouse on the riverfront. Bring your insect spray!',
+        shyEnjoyment: "Another fun place! Are you sure the water's sanitary? It looks questionable to me! -Cheyanne",
+        photos: ['./assets/Warrior Rock Lighthouse Trail.jpg', './assets/Warrior Rock Lighthouse Trail 1.jpg', './assets/Warrior Rock Lighthouse Trail 2.jpg']
     }
 ];
