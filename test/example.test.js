26abfaa75809e86e571a65cc9a320d65dd8738ff
Brien Thomas <thomasbrien94@gmail.com>
2022-05-10 17:37:41 -0700

    forgot to update TDD, passes now.

--- test/example.test.js
+++ test/example.test.js
@@ -9,7 +9,7 @@ const test = QUnit.test;
 test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = `<div class="landscape"><a href="/landscapes/?id=1"><h2>Forest</h2></a><img src="./assets/Forest.jpg"><p>Shy thinks this is a 10/10 spot to visit</p></div>`;
+    const expected = `<div class="landscape"><a href="/landscapes/?id=1"><h2>Latourell Falls</h2><img src="./assets/Latourell Falls.jpg"></a><p>A wooded area with hiking trails throughout, notable features include multiple waterfalls along the route!</p></div>`;
     
     //Act 
     // Call the function you're testing and set the result to a const
